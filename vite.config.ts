1a976e2b232e743316e17a638e513990ebc83538
Strieno <dr.strieno@gmail.com>
2026-08-29 18:37:50 +0300

    extra

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
+import {fileURLToPath,URL} from 'node:url';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/postcss';
 import {defineConfig} from 'vite';
-export default defineConfig({base:'/Hidden-Guest-OS/',plugins:[react()],css:{postcss:{plugins:[tailwindcss()]}}});
+export default defineConfig({base:'/Hidden-Guest-OS/',plugins:[react()],resolve:{alias:{'@':fileURLToPath(new URL('./',import.meta.url))}},css:{postcss:{plugins:[tailwindcss()]}}});
